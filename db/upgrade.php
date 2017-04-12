d37830003a50f80203ae8c0e6455ff44396553d8
valery <valery.fremaux@gmail.com>
2017-04-12 14:40:40 +0200

    fix travis

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,22 +1,18 @@
 <?php
-// This file keeps track of upgrades to 
-// the search block
+// This file is part of Moodle - http://moodle.org/
 //
-// Sometimes, changes between versions involve
-// alterations to database structures and other
-// major things that may break installations.
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// The upgrade function in this file will attempt
-// to perform all the necessary actions to upgrade
-// your older installtion to the current version.
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-// If there's something it cannot do itself, it
-// will tell you what you need to do.
-//
-// The commands in here will all be database-neutral,
-// using the functions defined in lib/ddllib.php
-
-defined('MOODLE_INTERNAL') || die();
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * @package   block_search
@@ -25,20 +21,22 @@ defined('MOODLE_INTERNAL') || die();
  * @author    Valery Fremaux (valery.fremaux@gmail.com)
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+defined('MOODLE_INTERNAL') || die();
 
 /**
  * Standard upgrade callback
  * @param int $oldversion
  */
 function xmldb_block_search_upgrade($oldversion=0) {
-    global $CFG, $THEME, $DB;
 
     $result = true;
 
-    // And upgrade begins here. For each one, you'll need one 
-    // block of code similar to the next one. Please, delete 
-    // this comment lines once this file start handling proper
-    // upgrade code.
+    /*
+     * And upgrade begins here. For each one, you'll need one
+     * block of code similar to the next one. Please, delete
+     * this comment lines once this file start handling proper
+     * upgrade code.
+     */
 
     return $result;
 }
